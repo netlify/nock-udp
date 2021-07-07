641f1b349499f03397460414df48a1357831f374
JGAntunes <joao@netlify.com>
2021-07-07 19:12:27 +0100

    feat: initial working version

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,8 @@ const { overrides } = require('@netlify/eslint-config-node')
 
 module.exports = {
   extends: '@netlify/eslint-config-node',
-  rules: {},
+  rules: {
+    'promise/prefer-await-to-callbacks': 'off',
+  },
   overrides: [...overrides],
 }
--- src/main.js
+++ src/main.js
@@ -1,7 +1,80 @@
 'use strict'
 
-const mainFunction = function () {
-  return true
+const { Socket } = require('dgram')
+
+const originalSocketSend = Socket.prototype.send
+
+let intercepts = {}
+
+const createScope = function (address, { persist = false } = {}) {
+  return {
+    used: false,
+    persist,
+    buffers: [],
+    offset: 0,
+    length: 0,
+    address,
+    clean: () => {
+      delete intercepts[address]
+    },
+  }
 }
 
-module.exports = { mainFunction }
+const validateBufferLength = function (buffer, offset, length) {
+  if (offset >= buffer.length) throw new Error('Offset into buffer too large')
+
+  if (offset + length > buffer.length) throw new Error('Offset + length beyond buffer length')
+}
+
+// eslint-disable-next-line max-params
+const mockSocketSend = function (buffer, offset, length, port, host, callback) {
+  validateBufferLength(buffer, offset, length)
+
+  const newBuffer = buffer.slice(offset, offset + length)
+
+  const address = `${host}:${port}`
+  if (!intercepts[address]) throw new Error(`Request sent to unmocked path: ${host}`)
+
+  const scope = intercepts[address]
+  scope.used = true
+  scope.buffers.push(newBuffer)
+  scope.offset = offset
+  scope.length = length
+
+  if (!scope.persist) delete intercepts[host]
+
+  if (callback) return callback(null, length)
+}
+
+mockSocketSend.mocked = true
+
+const isMocked = function () {
+  return Socket.prototype.send.mocked
+}
+
+const intercept = function (address, { persist = false, startIntercept = true } = {}) {
+  const scope = createScope(address, { persist })
+  intercepts[address] = scope
+  if (!isMocked() && startIntercept) interceptSocketSend()
+  return scope
+}
+
+const cleanAll = function ({ stopIntercept = true } = {}) {
+  intercepts = {}
+  if (isMocked() && stopIntercept) restoreSocketSend()
+}
+
+const restoreSocketSend = function () {
+  Socket.prototype.send = originalSocketSend
+}
+
+const interceptSocketSend = function () {
+  Socket.prototype.send = mockSocketSend
+}
+
+module.exports = {
+  intercept,
+  cleanAll,
+  restoreSocketSend,
+  interceptSocketSend,
+}
