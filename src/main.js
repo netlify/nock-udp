d11c937f70f7844b291e1361765efa85a02869f0
JGAntunes <joao@netlify.com>
2021-07-09 14:59:06 +0100

    feat: optionally allow messages to unknown hosts

--- src/main.js
+++ src/main.js
@@ -20,42 +20,53 @@ const createScope = function (address, { persist = false } = {}) {
   }
 }
 
-const validateBufferLength = function (buffer, offset, length) {
+const validateIncomingMsg = function (buffer, offset, length, address) {
   if (offset >= buffer.length) throw new Error('Offset into buffer too large')
 
   if (offset + length > buffer.length) throw new Error('Offset + length beyond buffer length')
-}
 
-// eslint-disable-next-line max-params
-const mockSocketSend = function (buffer, offset, length, port, host, callback) {
-  validateBufferLength(buffer, offset, length)
-
-  const newBuffer = buffer.slice(offset, offset + length)
-
-  const address = `${host}:${port}`
   if (!intercepts[address]) throw new Error(`Request sent to unmocked path: ${address}`)
-
-  const scope = intercepts[address]
-  scope.used = true
-  scope.buffers.push(newBuffer)
-  scope.offset = offset
-  scope.length = length
-
-  if (!scope.persist) delete intercepts[address]
-
-  if (callback) return callback(null, length)
 }
 
-mockSocketSend.mocked = true
+const getMockSocketSend = function ({ allowUnknown = false } = {}) {
+  // eslint-disable-next-line max-params
+  const mockSocketSend = function (buffer, offset, length, port, host, callback) {
+    const address = `${host}:${port}`
+    if (allowUnknown && !intercepts[address]) {
+      // We allow extraneous connections, fallback to original use
+      // eslint-disable-next-line no-invalid-this
+      originalSocketSend.call(this, buffer, offset, length, port, host, callback)
+      return
+    }
+
+    validateIncomingMsg(buffer, offset, length, address)
+
+    const newBuffer = buffer.slice(offset, offset + length)
+
+    const scope = intercepts[address]
+    scope.used = true
+    scope.buffers.push(newBuffer)
+    scope.offset = offset
+    scope.length = length
+
+    if (!scope.persist) delete intercepts[address]
+
+    if (callback) return callback(null, length)
+  }
+
+  mockSocketSend.mocked = true
+
+  return mockSocketSend
+}
 
 const isMocked = function () {
   return Boolean(Socket.prototype.send.mocked)
 }
 
-const intercept = function (address, { persist = false, startIntercept = true } = {}) {
+const intercept = function (address, { persist = false, startIntercept = true, allowUnknown = false } = {}) {
   const scope = createScope(address, { persist })
   intercepts[address] = scope
-  if (!isMocked() && startIntercept) interceptSocketSend()
+  if (!isMocked() && startIntercept) interceptSocketSend({ allowUnknown })
   return scope
 }
 
@@ -68,8 +79,8 @@ const restoreSocketSend = function () {
   Socket.prototype.send = originalSocketSend
 }
 
-const interceptSocketSend = function () {
-  Socket.prototype.send = mockSocketSend
+const interceptSocketSend = function ({ allowUnknown } = {}) {
+  Socket.prototype.send = getMockSocketSend({ allowUnknown })
 }
 
 module.exports = {
